c78605c1e67cad93f64b5b9a7d7ce1cc8d2a83f7
quangsam <quangsam150305@gmail.com>
2024-10-28 18:09:36 +0700

    hihi

---
+++ ci.js
@@ -0,0 +1,24 @@
+
+// ...slider product...
+let index = 0
+const rightbtnone = document.querySelector('.fa-chevron-right-one')
+const leftbtnone = document.querySelector('.fa-chevron-left-one')
+const imgNumberone = document.querySelectorAll('.slider-product-one-content-items')
+
+// console.log(rightbtnone)
+// console.log(leftbtnone)
+
+rightbtnone.addEventListener("click", function(){
+    index = index + 1;
+    if(index > imgNumberone.length - 1 ){
+        index = 0
+    }
+    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
+})
+leftbtnone.addEventListener("click" , function(){
+    index = index - 1;
+    if(index<=0){
+        index = imgNumberone.length - 1
+    }
+    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
+})
