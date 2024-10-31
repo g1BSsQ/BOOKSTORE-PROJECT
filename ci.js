90fa05539c8142c3a821d989f3bbed459e8edf4c
code <quangsam150305@gmail.com>
2024-10-31 08:22:03 +0700

    quang ngu nhu cho

--- ci.js
+++ ci.js
@@ -12,22 +12,22 @@ adresscloss.addEventListener("click", function(){
 let index = 0
 const rightbtnone = document.querySelector('.fa-chevron-right-one')
 const leftbtnone = document.querySelector('.fa-chevron-left-one')
-const imgNumberone = document.querySelectorAll('.slider-product-one-content-items')
+const slideNumberone = document.querySelectorAll('.slider-product-one-content-items')
 
 // console.log(rightbtnone)
 // console.log(leftbtnone)
 
 rightbtnone.addEventListener("click", function(){
-    index = index + 1;
-    if(index > imgNumberone.length - 1 ){
+    index = index+1
+    if(index > slideNumberone.length-1 ){
         index = 0
     }
     document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
 })
 leftbtnone.addEventListener("click" , function(){
-    index = index - 1;
+    index = index-1
     if(index<=0){
-        index = imgNumberone.length - 1
+        index = slideNumberone.length - 1
     }
     document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
 })
